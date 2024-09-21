9409af27b63644350ea98cff79fe75ad2a26ace1
noname <henrytan.next@gmail.com>
2024-09-21 16:55:52 +0700

    feat: add testcase for sync, async api

--- test/wrapper_test.exs
+++ test/wrapper_test.exs
@@ -1,11 +1,13 @@
 defmodule WrapperTest do
   use ExUnit.Case
+  use TDex.Timestamp
 
   test "login ok", _context do
     {:ok, conn} = TDex.Wrapper.taos_connect("127.0.0.1", 6030, "root", "taosdata", "tdex_test")
     {:ok, current_db} = TDex.Wrapper.taos_get_current_db(conn)
     assert current_db == "tdex_test"
-    TDex.Wrapper.taos_query(conn, "CREATE DATABASE IF NOT EXISTS test2")
+    ret = TDex.Wrapper.taos_query(conn, "CREATE DATABASE IF NOT EXISTS test2")
+    assert ret == {:ok, {0, 0, {}}, []}
     :ok = TDex.Wrapper.taos_select_db(conn, "test2")
     {:ok, current_db} = TDex.Wrapper.taos_get_current_db(conn)
     assert current_db == "test2"
@@ -22,6 +24,43 @@ defmodule WrapperTest do
   test "login failure", _context do
     {:error, "Invalid user"} = TDex.Wrapper.taos_connect("127.0.0.1", 6030, "rootx", "taosdata", "tdex_test")
     {:error, "Authentication failure"} = TDex.Wrapper.taos_connect("127.0.0.1", 6030, "root", "taosdata2", "tdex_test")
+  end
+
+  def recv_taos_async() do
+    receive do
+      msg -> msg
+    after
+      2000 ->
+        :timeout
+    end
+  end
+
+  test "async_api", _context do
+    {:ok, conn} = TDex.Wrapper.taos_connect("127.0.0.1", 6030, "root", "taosdata", "tdex_test")
+    :ok = TDex.Wrapper.taos_query_a(conn, "DROP TABLE IF EXISTS test1", self(), 1)
+    {:taos_reply, 1, {:ok, {0, 0, {}}}} = recv_taos_async()
+    :ok = TDex.Wrapper.taos_query_a(conn, "CREATE TABLE IF NOT EXISTS test1 (ts TIMESTAMP, text VARCHAR(255))", self(), 2)
+    {:taos_reply, 2, {:ok, {0, 0, {}}}} = recv_taos_async()
+    :ok = TDex.Wrapper.taos_query_a(conn, "SELECT * from test1", self(), 3)
+    {:taos_reply, 3, {:ok, {0, 0, {{~c"ts", 9}, {~c"text", 8}}}}} = recv_taos_async()
+    :ok = TDex.Wrapper.taos_close(conn)
+  end
+
+  @tag wip: true
+  test "sync_api", _context do
+    {:ok, conn} = TDex.Wrapper.taos_connect("127.0.0.1", 6030, "root", "taosdata", "tdex_test")
+    {:ok, {0, 0, {}}, []} = TDex.Wrapper.taos_query(conn, "DROP TABLE IF EXISTS test1")
+    {:ok, {0, 0, {}}, []} = TDex.Wrapper.taos_query(conn, "CREATE TABLE IF NOT EXISTS test1 (ts TIMESTAMP, text VARCHAR(255))")
+    {:ok, {0, 0, {{~c"ts", 9}, {~c"text", 8}}}, []} = TDex.Wrapper.taos_query(conn, "SELECT * from test1")
+     {:ok, stmt} = TDex.Wrapper.taos_stmt_prepare(conn, "INSERT INTO test1 VALUES (?, ?)")
+    spec = [{:ts, :TIMESTAMP}, {:text, {:VARCHAR, 255}}]
+    data = [
+      %{ts: ~X[2018-11-15 10:00:00.000Z], text: "hoang1"},
+      %{ts: ~X[2018-11-15 10:00:00.001Z], text: "hoang2"}
+    ]
+    {:ok, {0, 0, {}}, []} = TDex.Wrapper.taos_stmt_execute(stmt, spec, data)
+    :ok = TDex.Wrapper.taos_stmt_free(stmt)
+    :ok = TDex.Wrapper.taos_close(conn)
   end
 
 end
